0f5a0d2d3ed64f56b889f8babd919e1fa8edbb63
PhyxDown <264162213@qq.com>
2013-07-07 00:20:04 +0800

    phyxdown code 2

--- js/game/animation.js
+++ js/game/animation.js
@@ -10,8 +10,34 @@ function initStage(){
 
 function launch(){
 	//launch a square
+	for(var i = 0; i < 11; i++){
+		if(column[i] == 0 || square == column[i]){
+			column[i] = 0;
+		}
+		else{
+			var temp = square;
+			square = column[i];
+			column[i] = temp;
+			break;
+		}
+	}
+	return column;
 }
 
-function move(){
-	//move the square
+function rightMove(){
+	//move the square to the right
+	if(square != 11){
+		square++;
+		return "success";
+	}
+	return "failure";
+}
+
+function leftMove(){
+	//move the square to the left
+	if(square != 0){
+		square--;
+		return "success";
+	}
+	return "failure";
 }
--- js/game/init.js
+++ js/game/init.js
@@ -2,7 +2,23 @@ function init() {
     //Create a stage by getting a reference to the canvas
     stage = new createjs.Stage("demoCanvas");
     MatrixManager = new NS.MatrixManager();
-    initStage();
+
+    //0 for none, 1 for orange. 2 for yellow, 3 for green;
+    square = 1;
+    column = MatrixManager.getColumn(0);
+
+    showState()
+    
+    launch();
+
+    showState();
+    //Initialize Stage
+    //initStage();
+
     //Update stage will render next frame
     stage.update();
+}
+
+function showState(){
+	alert("square:"+square+"  "+"column:"+column);
 }
